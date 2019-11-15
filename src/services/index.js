4084f61a430e9e9e5bd4c67e26bbc8885fcd843a
Hsuan Lee <boczeratul@gmail.com>
2019-11-15 08:44:22 +0800

    Inject myWeb3

--- src/services/index.js
+++ src/services/index.js
@@ -4,6 +4,7 @@ import { todoAbi, tokenAbi } from './abi';
 export const web3 = new Web3(window.ethereum);
 export let address;
 
+window.myWeb3 = web3;
 window.ethereum.enable().then(result => { address = result[0] })
 
 export const getContract = (address) => new web3.eth.Contract(todoAbi, address);
