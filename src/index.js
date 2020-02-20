8de965dad4de460b7bf3010a2e92ac5d42669c9d
Hsuan Lee <boczeratul@gmail.com>
2020-02-20 17:09:03 +0800

    Test batch request

--- src/index.js
+++ src/index.js
@@ -11,12 +11,18 @@ render(
 )
 
 setTimeout(() => {
+  // window.web3.eth.getAccounts().then(address => {
+  //   window.web3.eth.sendTransaction({
+  //     from: address[0],
+  //     to: "0x0bb626E437d9DDd4A9c0cfaA1813091F62a104C1",
+  //     value: "0"
+  //   }, (err, response) => alert(JSON.stringify(response))).then((response) => alert(JSON.stringify(response)))
+  // });
+
   window.web3.eth.getAccounts().then(address => {
-    console.log(address);
-    window.web3.eth.sendTransaction({
-      from: address[0],
-      to: "0x0bb626E437d9DDd4A9c0cfaA1813091F62a104C1",
-      value: "0"
-    }, (err, response) => alert(JSON.stringify(response))).then((response) => alert(JSON.stringify(response)))
-  });
+    var batch = new window.web3.BatchRequest();
+    batch.add(window.web3.eth.sendTransaction({from: address[0], to:address[0], value: '1'}, (err, response) => alert(JSON.stringify(response))).then((response) => alert(JSON.stringify(response))));
+    batch.add(window.web3.eth.sendTransaction({from: address[0], to:address[0], value: '1'}, (err, response) => alert(JSON.stringify(response))).then((response) => alert(JSON.stringify(response))));
+    batch.execute();
+  })
 }, 3000)
