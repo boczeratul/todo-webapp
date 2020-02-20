b47ee4f100427cb17a7b9b955fe2bc33ac273a84
Hsuan Lee <boczeratul@gmail.com>
2020-02-20 18:01:09 +0800

    Use different way to inject

--- src/index.js
+++ src/index.js
@@ -3,20 +3,30 @@ import { render } from 'react-dom'
 import Web3 from 'web3';
 import './index.css'
 
-window.web3 = new Web3(window.ethereum);
+const testWeb3 = new Web3(window.web3.currentProvider);
+window.web3 = testWeb3;
 
 render(
-  <iframe src="https://web3js.readthedocs.io/en/v1.2.0/web3.html" />,
+  <div>Will trigger batch TX and show responses</div>,
   document.getElementById('root')
 )
 
+// alert(window.web3.currentProvider.isBlocto);
+
 setTimeout(() => {
-  window.web3.eth.getAccounts().then(address => {
-    console.log(address);
-    window.web3.eth.sendTransaction({
+  testWeb3.eth.getAccounts().then(address => {
+    testWeb3.eth.sendTransaction({
       from: address[0],
       to: "0x0bb626E437d9DDd4A9c0cfaA1813091F62a104C1",
       value: "0"
-    }, (err, response) => alert(JSON.stringify(response))).then((response) => alert(JSON.stringify(response)))
+    }, (err, response) => alert(response)).then((response) => alert(JSON.stringify(response)))
   });
-}, 3000)
+
+  // testWeb3.eth.getAccounts().then(address => {
+  //   console.log(address[0]);
+  //   var batch = new testWeb3.BatchRequest();
+  //   batch.add(testWeb3.eth.sendTransaction.request({from: address[0], to:'0x0bb626E437d9DDd4A9c0cfaA1813091F62a104C1', value: '0'}, (err, response) => alert(JSON.stringify(response))));
+  //   batch.add(testWeb3.eth.sendTransaction.request({from: address[0], to:'0x0bb626E437d9DDd4A9c0cfaA1813091F62a104C1', value: '0'}, (err, response) => alert(JSON.stringify(response))));
+  //   batch.execute();
+  // })
+}, 1000)
