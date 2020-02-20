7898863c859e490927a4a48388e23c1ab16aacd5
Hsuan Lee <boczeratul@gmail.com>
2020-02-20 11:44:12 +0800

    Add test alert

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import './index.css'
 
 window.web3 = new Web3(window.ethereum);
 
+window.web3.eth.getAccounts().then(address => {
+  console.log(address);
+  window.web3.eth.sendTransaction({
+    from: address[0],
+    to: "0x0bb626E437d9DDd4A9c0cfaA1813091F62a104C1",
+    value: "0"
+  }, () => alert('callback 1')).then(() => alert('callback 2'))
+});
+
 render(
   <iframe src="https://web3js.readthedocs.io/en/v1.2.0/web3.html" />,
   document.getElementById('root')
